ec285179d8b2415d1a87b1db4281d1ffea54ba7f
Ricardo <ricardo.rossini.dev@outlook.com>
2020-10-16 18:07:06 -0300

    regras de required + tratmento de erros (tem um bug! - CORRIGIR

--- Validacao_Formulario/js/script.js
+++ Validacao_Formulario/js/script.js
@@ -3,28 +3,28 @@ let validator = {
         event.preventDefault();
         let send = true;
         let inputs = form.querySelectorAll('input');
-
+        validator.clearErrors();
         for(let i = 0; i < inputs.length; i++) {
             let input = inputs[i];
             //console.log(input);
             let check = validator.checkInput(input);
-
             if(check !== true) {
                 send = false;
-                console.log(check);
+                //console.log(check);
+                validator.showError(input, check);
             } 
         }
-       // send = false;
-        //send ? form.submit() : 
+        // send = false;
+        if(send) {
+            form.submit();
+        }
     },
     checkInput:(input) => {
         let rules = input.getAttribute('data-rules');
         if(rules !== null) {
             rules = rules.split('|');
-
             for(let k in rules) {
                 let rDetails = rules[k].split('=');
-
                 switch(rDetails[0]) {
                     case 'required':
                         if(input.value == '') {
@@ -41,9 +41,24 @@ let validator = {
         else{
             return true;
         }
+    },
+    showError:(input, error) => {
+        input.style.borderColor = '#FF0000';
+        let errorElement = document.createElement('div');
+        errorElement.classList.add('error');
+        errorElement.innerHTML = error;
+        input.parentElement.insertBefore(errorElement, input.ElementSibling);
+    },
+    clearErrors:() => {
+        let inputs =form.querySelectorAll('input');
+        for(let i = 0; i < inputs.length; i++) {
+            inputs.style = '';
+        }
+        let errorElements = document.querySelectorAll('.error');
+        for(let i = 0; i < errorElements.length; i++) {
+            errorElements[i].remove();
+        }
     }
 }
-
 let form = document.querySelector('.valida');
-
 form.addEventListener('submit', validator.handleSubmit);
