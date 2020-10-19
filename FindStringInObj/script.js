047e5b0a409ce98aea9cd17fd896521a6d6c9628
Ricardo <ricardo.rossini.dev@outlook.com>
2020-10-19 11:48:13 -0300

    find string in obj final commit

--- FindStringInObj/script.js
+++ FindStringInObj/script.js
@@ -1,3 +1,20 @@
-function exibirPropriedades(obj) {
-    
+//Criar um metodo para ler as propriedades de um objeto
+//e exibir somente as propriedades do tipo string
+
+const filme = {
+    titulo: 'V for Vendetta',
+    ano: 2005,
+    diretor: 'James McTeigue',
+    peronagem: 'V'
 }
+
+function exibirPropriedades(obj) {
+    for(let i in obj){
+        if(typeof obj[i] === 'string'){
+            console.log(i, obj[i]);
+        }
+    }
+}
+
+
+//"Vi Veri Veniversum Vivus Vici". Translated as "By the power of truth, I, while living, have conquered the universe."
