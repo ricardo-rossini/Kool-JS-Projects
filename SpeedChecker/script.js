3d66de3a53a2662aa4570d571e14cfb09bb708e3
Ricardo <ricardo.rossini.dev@outlook.com>
2020-10-18 22:42:48 -0300

    verifica velo final commit

--- SpeedChecker/script.js
+++ SpeedChecker/script.js
@@ -1,3 +1,22 @@
+//velocidade maxima 70 km/h
+// a cada 5Km/h a mais que o limite = 1 ponto 
+// se ponto for maior que 12 => 'Carteira Suspensa'
+
 function verificaVelocidade(velocidade) {
-    
+    const velMax = 70;
+    if(velocidade <= velMax) {
+        console.log('OK!');
+    }
+    if(velocidade > 70 && velocidade < 75) {
+        console.log('voce foi multado!');
+    }
+    if(velocidade >= 75){
+        let pontos = 0;
+        let velMaior = velocidade - velMax;
+        pontos = Math.floor(velMaior / 5);
+        console.log(`voce recebeu ${pontos} pnto na carteira`);
+        if(pontos >= 12){
+            console.log('Sua carteira foi suspensa');
+        }
+    }
 }
