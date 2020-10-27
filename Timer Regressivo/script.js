74f4eb1b2eb4f2eb6b11f5fe6081a7c80ab9186f
Ricardo <ricardo.rossini.dev@outlook.com>
2020-10-27 01:59:11 -0300

    Timer Regressivo FINAL COMMIT

--- Timer Regressivo/script.js
+++ Timer Regressivo/script.js
@@ -1,18 +1,30 @@
 const newYear = '1 Jan 2021';
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('mins');
+const secsEl = document.getElementById('secs');
 
 function countdown() {
     const newYearDate = new Date(newYear);
     const currentDate = new Date();
-
+    //Math!!!
     const totalSeconds = (newYearDate - currentDate) / 1000;
     const days = Math.floor(totalSeconds / 3600 / 24);
     const hours = Math.floor(totalSeconds / 3600) % 24;
     const mins = Math.floor(totalSeconds / 60) % 60;
     const secs = Math.floor(totalSeconds % 60);
-     
-    console.log(days, hours, mins, secs);
-}
-// INICIAL CALL
+    //console.log(days, hours, mins, secs);
 
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secsEl.innerHTML = formatTime(secs)
+}
+
+function formatTime(time) {
+    return time < 10 ? (`0${time}`) : time;
+}
+
+// INICIAL CALL
 countdown();
-setInterval(countdown, 1000);
+//setInterval(countdown, 1000);
