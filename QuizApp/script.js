f3a2f5a3c111ca130f22e819f800ede73ea24b96
Ricardo <ricardo.rossini.dev@outlook.com>
2020-10-28 01:47:56 -0300

    proto function - working

--- QuizApp/script.js
+++ QuizApp/script.js
@@ -43,4 +43,23 @@ const quizData = [
         d: 'Monica',
         correct: 'b'
     }
-]
+];
+
+let currentQuestion = 0;
+const questionEl = document.getElementById('question');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+
+loadQuiz();
+
+function loadQuiz() {
+    const currentQuizData = quizData[currentQuestion];
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+    currentQuestion++;
+}
