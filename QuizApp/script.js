9ab3c47dc8f82de32cab1e15c454f653d1a8ad46
Ricardo <ricardo.rossini.dev@outlook.com>
2020-10-27 22:32:08 -0300

    quizApp basic html

---
+++ QuizApp/script.js
@@ -0,0 +1,46 @@
+const quizData = [
+    {
+        question: 'Qual a cidades mais ao Sul do Brasil?',
+        a: 'Oiapoque',
+        b: 'Sao Paulo',
+        c: 'Chui',
+        d: "Pelotas",
+        correct: 'c'
+    },
+
+    {
+        question: 'Como e conhecido o melhor amigo do detetive Sherlock Holmes?',
+        a: 'Samuel',
+        b: 'Watson',
+        c: 'Percy',
+        d: 'Lestrad',
+        correct: 'b'
+    },
+
+    {
+        question: 'Qual o peixe mais consumido no Brasil?',
+        a: 'tilapia',
+        b: 'sardinha',
+        c: 'pescado',
+        d: 'namorado',
+        correct: 'a'
+    },
+
+    {
+        question: 'Qual a velocidade do som?',
+        a: '1432km/h',
+        b: '2341km/h',
+        c: '1342km/h',
+        d: '1234km/h',
+        correct: 'd'
+    },
+
+    {
+        question:'Qual personagem de Mauricio de oua eh o dono do Bidu?',
+        a: 'Cebolinha',
+        b: 'Franjinha',
+        c: 'Magali',
+        d: 'Monica',
+        correct: 'b'
+    }
+]
