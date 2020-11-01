8dc062cce56704065345af8c72fa5e7cd0dc84af
Ricardo <ricardo.rossini.dev@outlook.com>
2020-10-31 22:22:48 -0300

    adding fetch - TheMealDB API

--- App de Receitas/script.js
+++ App de Receitas/script.js
@@ -1,11 +1,11 @@
-function getRandomMeal() {
-
+async function getRandomMeal() {
+    const random = fetch('https://www.themealdb.com/api/json/v1/1/random.php');
 }
 
-function getMealById(id) {
-
+async function getMealById(id) {
+    const meal = fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + id);
 }
 
-function getMealsBySearch(term) {
-
+async function getMealsBySearch(term) {
+    const meals = fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=' + term);
 }
