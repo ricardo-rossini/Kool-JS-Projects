fd776074d5e9f4618b619d540f79275bcf7b7bd2
Ricardo <ricardo.rossini.dev@outlook.com>
2020-11-01 00:50:57 -0300

    addMeal(randomRecipe)

--- App de Receitas/script.js
+++ App de Receitas/script.js
@@ -1,3 +1,5 @@
+const meals = document.getElementById('meals');
+
 getRandomMeal();
 
 async function getRandomMeal() {
@@ -5,7 +7,7 @@ async function getRandomMeal() {
     const responseData = await resp.json();
     const randomMeal = responseData.meals[0];
     console.log(randomMeal);
-    loadRandomMeal();
+    addMeal(randomMeal, true);
 }
 
 async function getMealById(id) {
@@ -14,4 +16,28 @@ async function getMealById(id) {
 
 async function getMealsBySearch(term) {
     const meals = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=' + term);
+}
+
+function addMeal(mealData, random = false) {
+    const meal = document.createElement('div');
+    meal.classList.add('meal');
+
+    meal.innerHTML = `
+        <div class="meal">
+            <div class="meal-header">
+                ${random ? `<span class="random">
+                    Random Recipe
+                </span>` : ''}
+                <img src="${mealData.strMealThumb}" alt="${mealData.strMeal}">
+            </div>
+            <div class="meal-body">
+                <h4>${mealData.strMeal}</h4>
+                <button class="fav-btn active">
+                    <i class="far fa-heart"></i>
+                </button>
+            </div>
+        </div>
+    `;
+
+    meals.appendChild(meal);
 }
