71adbb9252212f522bf9b31f56fe372e43b8f3c5
Ricardo <ricardo.rossini.dev@outlook.com>
2020-11-08 22:32:53 -0300

    adding textArea eventListener + maked

--- Notes App/script.js
+++ Notes App/script.js
@@ -9,3 +9,10 @@ editBtn.addEventListener('click', () => {
     main.classList.toggle('hidden');
     textArea.classList.toggle('hidden');
 });
+
+textArea.addEventListener('input', (e) => {
+    const { value } = e.target;
+    main.innerHTML = marked(value);
+});
+
+
