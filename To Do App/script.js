63783f20ede1c23eee960fe53b158544ab8aed70
Ricardo <ricardo.rossini.dev@outlook.com>
2020-11-12 21:41:40 -0300

    just some few adjustments

---
+++ To Do App/script.js
@@ -0,0 +1,12 @@
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const todo = input.value;
+    
+    if(todo) {
+        
+    }
+});
