2de0922421b2a7df99be3fb8bd1e5f7d1fbe6729
Ricardo <ricardo.rossini.dev@outlook.com>
2020-11-13 02:18:19 -0300

    ToDo App Final Commit

--- To Do App/script.js
+++ To Do App/script.js
@@ -1,29 +1,61 @@
 const form = document.getElementById('form');
 const input = document.getElementById('input');
-const todos = document.getElementById('todos');
+const todosUL = document.getElementById('todos');
+const todos = JSON.parse(localStorage.getItem('todos'));
+
+if(todos) {
+    todos.forEach(todo => {
+        addTodo(todo);
+    });
+}
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     addTodo();
 });
 
-function addTodo() {
-    const todoText = input.value;
+function addTodo(todo) {
+    let todoText = input.value;
     
+    if(todo) {
+        todoText = todo.text;
+    }
+
     if(todoText) {
         const todoEl = document.createElement('li');
+        if(todo && todo.completed) {
+            todoEl.classList.add('completed');
+        }
+
         todoEl.innerHTML = todoText;
 
         todoEl.addEventListener('click', () => {
             todoEl.classList.toggle('completed');
+            updateLS();
         });
 
         todoEl.addEventListener('contextmenu', (e) => {
             e.preventDefault();
             todoEl.remove();
+            updateLS();
         });
 
-        todos.appendChild(todoEl);
+        todosUL.appendChild(todoEl);
         input.value = "";
+
+        updateLS();
     }
+}
+
+function updateLS() {
+    const todosEl = document.querySelectorAll('li');
+    const todos = [];
+    todosEl.forEach(todoEl => {
+        todos.push({
+            text: todoEl.innerText,
+            completed: todoEl.classList.contains('completed')
+        });
+    });
+    
+    localStorage.setItem('todos', JSON.stringify(todos));
 }
