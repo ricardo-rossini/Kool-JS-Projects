4b89cd1c0acc3feb307ac57cda152928741fd04a
Ricardo <ricardo.rossini.dev@outlook.com>
2020-11-13 20:54:39 -0300

    adding The Movie DB API

---
+++ Movie App/script.js
@@ -0,0 +1,17 @@
+const APIURL = 'https://api.themoviedb.org/3/discover/movie?api_key=8c5e3d4cce9e66b58c25c2bd8b9db469&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1';
+const IMGPATH = 'https://image.tmdb.org/t/p/w1280';
+async function getMovies() {
+    const resp = await fetch(APIURL);
+    const respData = await resp.json();
+
+    console.log(respData);
+
+    respData.results.forEach(movie => {
+        const img = document.createElement('img');
+        img.src = IMGPATH + movie.poster_path;
+        document.body.appendChild(img);
+    });
+    return respData;
+}
+
+getMovies();
