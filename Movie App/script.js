4a7ed3903c41dc3995bc6cf83017f16c1b8b34ee
Ricardo <ricardo.rossini.dev@outlook.com>
2020-11-15 18:00:24 -0300

    adding search feature

--- Movie App/script.js
+++ Movie App/script.js
@@ -1,27 +1,39 @@
 const APIURL = 'https://api.themoviedb.org/3/discover/movie?api_key=8c5e3d4cce9e66b58c25c2bd8b9db469&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1';
 const IMGPATH = 'https://image.tmdb.org/t/p/w1280';
-async function getMovies() {
-    const resp = await fetch(APIURL);
-    const respData = await resp.json();
-    const main = document.querySelector('main');
+const SEARCHAPI = 'https://api.themoviedb.org/3/search/movie?api_key=8c5e3d4cce9e66b58c25c2bd8b9db469&query=';
+const main = document.querySelector('main');
+const form = document.querySelector('form');
+const search = document.getElementById('search');
 
+//getting fav movies
+getMovies(APIURL);
+
+async function getMovies(url) {
+    const resp = await fetch(url);
+    const respData = await resp.json();   
     console.log(respData);
 
-    respData.results.forEach(movie => {
+    showMovies(respData.results);
+}
+
+function showMovies(movies) {
+    //clear main
+    main.innerHTML = '';
+    //getting movies
+    movies.forEach(movie => {
         const movieEl = document.createElement('div');
         movieEl.classList.add('movie');
         
         movieEl.innerHTML = `
-        <img src="${IMGPATH + movie.poster_path}" alt="${movie.title}">
-        <div class="movie-info">
-            <h3>${movie.title}</h3>
-            <span class="${getClassByRate(movie.vote_average)}">${movie.vote_average}</span>
-        </div>
+            <img src="${IMGPATH + movie.poster_path}" alt="${movie.title}">
+            <div class="movie-info">
+                <h3>${movie.title}</h3>
+                <span class="${getClassByRate(movie.vote_average)}">${movie.vote_average}</span>
+            </div>
         `;
 
         main.appendChild(movieEl);
     });
-    return respData;
 }
 
 function getClassByRate(vote) {
@@ -36,4 +48,13 @@ function getClassByRate(vote) {
     }
 }
 
-getMovies();
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+    
+    if(searchTerm) {
+        getMovies(SEARCHAPI + searchTerm);
+        search.value = '';
+    }
+});
