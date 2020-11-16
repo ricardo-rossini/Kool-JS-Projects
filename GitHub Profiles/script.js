51111f7704daadb846ee06d23376a10b657d5b16
Ricardo <ricardo.rossini.dev@outlook.com>
2020-11-16 12:47:43 -0300

    adding search user script

--- GitHub Profiles/script.js
+++ GitHub Profiles/script.js
@@ -1,4 +1,7 @@
 const APIURL = "https://api.github.com/users/";
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
 
 async function getUser(user) {
     const resp = await fetch(APIURL + user);
@@ -8,5 +11,33 @@ async function getUser(user) {
 }
 
 function createUserCard(user) {
+    const cardHTML = `
+        <div class="card">
+            <div>
+                <img src="${user.avatar_url}" alt="${user.name}" />
+            </div>
+            <div>
+                <h2>${user.name}</h2>
+                <p>${user.bio}</P>
 
+                <ul>
+                    <li>${user.followers}</li>
+                    <li>${user.following}</li>
+                    <li>${user.public_repos}</li>
+                </ul>
+            </div>
+        </div>
+    `;
+    main.innerHTML = cardHTML;
 }
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const user = search.value;
+    
+    if(user) {
+        getUser(user);
+        search.value = '';
+    }
+});
