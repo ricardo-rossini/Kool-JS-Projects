378e657ec2e2bd1c743b4a33b99e3ec01bdc58db
Ricardo <ricardo.rossini.dev@outlook.com>
2020-11-20 19:25:43 -0300

    adding drawLine()

--- Drawing App/script.js
+++ Drawing App/script.js
@@ -7,24 +7,30 @@ const ctx = canvas.getContext('2d');
 
 let color = 'black';
 let size = 20;
-let x = 50;
-let y = 50;
+let x = undefined;
+let y = undefined;
 let isPressed = false;
 
-canvas.addEventListener('mousedown', () => {
+canvas.addEventListener('mousedown', (e) => {
     isPressed = true;
+    x = e.offsetX;
+    y = e.offsetY;
 });
 
 canvas.addEventListener('mouseup', () => {
     isPressed = false;
+    x = undefined;
+    y = undefined;
 });
 
 canvas.addEventListener('mousemove', (e) => {
     if(isPressed){
-        const x = e.offsetX;
-        const y = e.offsetY;
+        const x2 = e.offsetX;
+        const y2 = e.offsetY;
 
-        drawCircle(x,y);
+        drawLine(x, y, x2, y2);
+        x = x2;
+        y = y2;
     } 
 });
 
@@ -33,6 +39,14 @@ function drawCircle(x, y) {
     ctx.arc(x, y, size, 0, Math.PI * 2);
     ctx.fillStyle = color;
     ctx.fill();
+}
+
+function drawLine(x1, y1, x2, y2) {
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.strokeStyle = color;
+    ctx.stroke();
 }
 
 increaseBtn.addEventListener('click', () => {
