5ac76873495dfd7bf1ffd61b386e9d5627e31679
Ricardo <ricardo.rossini.dev@outlook.com>
2020-11-19 23:32:39 -0300

    adding drawCircle() working!

--- Drawing App/script.js
+++ Drawing App/script.js
@@ -1,15 +1,27 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-let size = 30;
+let size = 20;
+let x = 50;
+let y = 50;
 
-canvas.addEventListener('mousedown', () => {
+canvas.addEventListener('mousedown', (e) => {
+    const x = e.offsetX;
+    const y = e.offsetY;
 
+    drawCircle(x,y); 
 });
 
 function drawCircle(x, y) {
     ctx.beginPath();
     ctx.arc(x, y, size, 0, Math.PI * 2);
     ctx.stroke();
+    ctx.fill();
 }
 
-drawCircle(50, 50);
+/* function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawCircle(x,y); //++ will animate the axe
+    requestAnimationFrame(draw);
+}
+
+draw(); */
