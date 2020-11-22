50134594883588553cf196dcfdebba7d2d97e313
Ricardo <ricardo.rossini.dev@outlook.com>
2020-11-21 23:15:31 -0300

    adding basic randomizers functions

---
+++ Password Generator/script.js
@@ -0,0 +1,32 @@
+const pwEl = document.getElementById('pw');
+const copyEl = document.getElementById('copy');
+const lenEl = document.getElementById('len');
+const upperEl = document.getElementById('upper');
+const lowerEl = document.getElementById('lower');
+const numberEl = document.getElementById('number');
+const symbolEl = document.getElementById('symbol');
+const generateEl = document.getElementById('generate');
+const upperLetters = 'ABCDEFGHIJHLMNOPQRSTUVWXYZ';
+const lowerLetters = 'abcdefghijklmnopqrstuvwxyz';
+const numbers = '0123456789';
+const symbols = '!@#$%^&*()_+';
+
+function getLowercase() {
+    return loweLetters[Math.floor(Math.random() * loweLetters.length)];
+}
+
+function getUppercase() {
+    return upperLetters[Math.floor(Math.random() * upperLetters.length)];
+}
+
+function getNumber() {
+    return numbers[Math.floor(Math.random() * numbers.length)];
+}
+
+function getSymbol() {
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+function generatePassword() {
+    
+}
