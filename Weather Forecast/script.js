f585dc59967a3d966be90387433b59ff76de8467
Ricardo <ricardo.rossini.dev@outlook.com>
2020-11-23 23:48:31 -0300

    adding getWeatherByCity() Working

--- Weather Forecast/script.js
+++ Weather Forecast/script.js
@@ -1,11 +1,46 @@
 const apikey = "3265874a2c77ae4a04bb96236a642d2f";
 
-const url = (location) => `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apikey}`
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
 
-async function getWeatherByLocation(location) {
-    const resp = fetch(url (location), { origin: "cors" });
-    const respData = (await resp).json();
+
+const url = (city) =>
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
+
+async function getWeatherByCity(city) {
+    const resp = await fetch(url(city), {origin:"cors"});
+    const respData = await resp.json();
+    
     console.log(respData);
+
+    addWeatherToPage(respData);
 }
 
-getWeatherByLocation('London');
+function addWeatherToPage(data) {
+    const temp = KtoC(data.main.temp);
+
+    const weather = document.createElement('div');
+    weather.classList.add('weater');
+
+    weather.innerHTML = `
+        <h2>${temp}°C</h2>
+        <p>${search.value}</p>
+    `;
+
+    main.appendChild(weather);
+}
+
+function KtoC(K) {
+    return Math.floor(K - 273.15);
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const city = search.value;
+    
+    if (city) {
+        getWeatherByCity(city);
+    }
+});
